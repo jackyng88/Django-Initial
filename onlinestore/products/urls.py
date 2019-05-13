99dd97f59e44b3c04d25a441521d29e9e061b442
Jacky <jacky.ng9188@gmail.com>
2019-05-13 17:04:26 -0400

    Simple API using only Django. Implemented simple views for endpoints

--- onlinestore/products/urls.py
+++ onlinestore/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView
+from .views import product_detail, product_list
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail')
+    # Typically endpoints for APIs are like /api/products/ for example.
+    # Look at /onlinestore/urls.py for the urlpath for this endpoint.
+    path('products/', product_list, name='product-list'),
+    path('products/<int:pk>/', product_detail, name='product-detail'),
 ]
--- onlinestore/products/views.py
+++ onlinestore/products/views.py
@@ -1,8 +1,45 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-
+from django.http import JsonResponse
 from .models import Product, Manufacturer
 
+
+def product_list(request):
+    products = Product.objects.all()
+    # You can also take a slice of the objects i.e. [:30]
+    data = {'products': list(products.values())}     # 'pk', 'name'
+    response = JsonResponse(data)
+    
+    return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {'product': 
+                    {
+                    'name': product.name,
+                    'manufacturer': product.manufacturer.name,
+                    'description': product.description,
+                    'photo': product.photo.url,
+                    'price': product.price,
+                    'shipping_cost': product.shipping_cost,
+                    'quantity': product.quantity,
+                    }
+                }
+        response = JsonResponse(data)
+     
+    except Product.DoesNotExist:   
+         response = JsonResponse({
+             'error': {
+                 'code': 404,
+                 'message': 'product not found!'
+             }
+         }, status=404)
+
+    return response
+
+
+
+""" from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 class ProductDetailView(DetailView):
     model = Product
@@ -11,4 +48,5 @@ class ProductDetailView(DetailView):
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/product_list.html'
+    template_name = 'products/product_list.html' """
+
